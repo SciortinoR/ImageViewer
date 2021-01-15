33d5180c433134ce92d2cd98ac83f1738a11e861
Roberto Sciortino <roberto.sciortino@mail.mcgill.ca>
2021-01-15 17:12:17 -0500

    Added Status and Permissions enum classes

---
+++ enums.py
@@ -0,0 +1,12 @@
+import enum
+
+# Enum statuc codes
+class Status(enum.Enum):
+    SUCCESS = 0
+    ERROR = 1
+
+# Enum image permissions
+class Permissions(enum.Enum):
+    PRIVATE = 0
+    PUBLIC = 1
+
